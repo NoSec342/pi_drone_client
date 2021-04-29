5330f4185c6e2c08db08b89731ac0b32e7746d6d
NoSec342 <57366092+NoSec342@users.noreply.github.com>
2021-04-29 11:14:06 +0300

    No pi_drone_client service needed... LOL

--- install.sh
+++ install.sh
@@ -9,13 +9,8 @@ make
 cd ..
 if [ $EUID == "0" ];then
     ln -s "$SOURCE_DIR/build/pi_drone_client" /usr/bin/pi_drone_client
-    systemctl link ./pidrn_clt.service
-    systemctl enable pidrn_clt.service
-    systemctl start pidrn_clt.service
+    
 else
     sudo ln -s "$SOURCE_DIR/build/pi_drone_client" /usr/bin/pi_drone_client
-    sudo systemctl link ./pidrn_clt.service
-    sudo systemctl enable pidrn_clt.service
-    sudo systemctl start pidrn_clt.service
 fi
 echo "Installation Done!"
